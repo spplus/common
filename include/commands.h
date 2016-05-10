3e7e8ff4034e480260f60a5c64769fdfab398410
spplus <yleesun@163.com>
2016-05-10 17:36:01 +0800

    增加命令
    增系统名称配置

--- include/commands.h
+++ include/commands.h
@@ -10,12 +10,17 @@
 #ifndef __COMMANDS_H__
 #define __COMMANDS_H__
 
+// 连接建立
+#define				CMD_CONNECTED				1000
+
+// 连接断开
+#define				CMD_DISCONNECTED			1001
 
 // 用户登录
-#define				CMD_USER_LONGIN				1000
+#define				CMD_USER_LONGIN					2000
 
 // 获取设备状态
-#define				CMD_DEV_STATE					2000
+#define				CMD_DEV_STATE					2001
 
 #endif
 
